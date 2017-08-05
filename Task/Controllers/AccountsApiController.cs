826467f8e376d8069c887a527808d270d86e9db1
AbbasTallaq <30746350+AbbasTallaq@users.noreply.github.com>
2017-08-05 11:52:10 +0300

    Update AccountsApiController.cs

--- Task/Controllers/AccountsApiController.cs
+++ Task/Controllers/AccountsApiController.cs
@@ -92,6 +92,7 @@ namespace Task.Controllers
         /// </summary>
         /// <param name="TaskDTO"></param>
         /// <returns></returns>
+        //Test commect
 
         // POST: api/Accounts1
         [ResponseType(typeof(TaskDTO))]
